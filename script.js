5b7976c04005692718cc8496cb01ce9452a54917
HaveSechs <128012859+HaveSechs@users.noreply.github.com>
2023-04-30 15:40:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,23 @@ class Example extends Phaser.Scene {
     }
 
     create () {
+        this.score = 0;
         this.add.image(0, 0, 'background');
         this.player = this.physics.add.sprite(0, 500, 'player');
+        this.sd = this.add.text(0, 0, this.score);
         this.player.body.onCollide = true;
-        this.interval = 500
         this.asteroids = [this.physics.add.sprite(Math.random() * 800, 0, 'asteroid')]
         this.physics.add.collider(this.player, this.asteroids[0]);
 
         this.physics.world.on('collide', () => {
-            document.body.innerHTML = "You lost bozo<br><button onclick=\"window.location.reload();\">try again</button>";
+            for (let a = 0; a < this.asteroids.length; a++) {
+                this.asteroids[a].destroy(true);
+            }
+            document.body.innerHTML = `you died bozo<br>
+            <button onclick="window.location.reload();">try again</button>`
         });
         
-        setInterval(this.new_asteroid.bind(this), 500)
+        setInterval(this.new_asteroid.bind(this), 750)
     }
 
     new_asteroid () {
@@ -36,7 +41,14 @@ class Example extends Phaser.Scene {
         const maxX = this.game.config.width - this.player.width / 2;
 
         for (let a = 0; a < this.asteroids.length; a++) {
-            this.asteroids[a].y += 3
+            this.asteroids[a].y += 3;
+            if (this.asteroids[a].y > 600) {
+                this.asteroids[a].destroy(true);
+                this.asteroids.splice(a, 1);
+                this.score += 1;
+                console.log(this.score);
+                this.sd.setText(this.score);
+            }
         }
         
         if (aKey.isDown && this.player.x > minX) {
